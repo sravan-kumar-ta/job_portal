df29eb45b4235ed397550d25397118684a624c41
sravan-kumar-ta <sravan0095@gmail.com>
2022-05-20 20:14:55 +0530

    Job model added

--- employer/models.py
+++ employer/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Job(models.Model):
+    job_title = models.CharField(max_length=120)
+    company = models.CharField(max_length=120)
+    location = models.CharField(max_length=120)
+    salary = models.PositiveIntegerField(null=True)
+    experience = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.job_title
